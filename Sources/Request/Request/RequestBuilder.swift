14128f567060b709c05aef4cd532a8270b7763b1
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2019-10-19 02:42:55 +0300

    Add buildIf realization

--- Sources/Request/Request/RequestBuilder.swift
+++ Sources/Request/Request/RequestBuilder.swift
@@ -10,14 +10,27 @@ import Foundation
 @_functionBuilder
 public struct RequestBuilder {
     public static func buildBlock(_ params: RequestParam...) -> RequestParam {
+
+        let resultParams = params.filter { $0.children == nil } + params.compactMap { $0.children }.joined()
         // Multiple Urls
-        if params.filter({ $0.type == .url }).count > 1 {
+        if resultParams.filter({ $0.type == .url }).count > 1 {
             fatalError("You cannot specify more than 1 `Url`")
         }
         // Missing Url
-        if params.filter({ $0.type == .url }).count < 1 {
+        if resultParams.filter({ $0.type == .url }).count < 1 {
             fatalError("You must have a `Url`")
         }
-        return CombinedParams(children: params)
+        return CombinedParams(children: resultParams)
+    }
+
+    public static func buildBlock(_ param: RequestParam) -> RequestParam {
+        return param
+    }
+
+    public static func buildIf(_ param: RequestParam?) -> RequestParam {
+        if let param = param {
+            return param
+        }
+        return CombinedParams(children: [])
     }
 }
