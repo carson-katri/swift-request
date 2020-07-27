0c57c5b62690a99a62d97aa968d417d65124362d
Carson Katri <carson.katri@gmail.com>
2020-07-27 19:29:43 -0400

    Fix CI build

--- Sources/Request/Request/Extra/RequestGroup+Combine.swift
+++ Sources/Request/Request/Extra/RequestGroup+Combine.swift
@@ -12,7 +12,7 @@ extension RequestGroup: Publisher {
     public typealias Output = [URLSession.DataTaskPublisher.Output]
     public typealias Failure = Error
     
-    public func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
         Publishers.Sequence(sequence: requests)
             .flatMap { $0 }
             .collect()
