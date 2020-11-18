f4513dd36dabd113689f0898aca4e7f660a2f7bc
brennoumobi <brenno@umobi.com.br>
2020-11-18 18:19:11 -0300

    Added FormBuilder

--- Sources/Request/Request/FormParam/Form.swift
+++ Sources/Request/Request/FormParam/Form.swift
@@ -10,17 +10,8 @@ import Foundation
 public struct Form: FormParam {
     private let params: [FormParam]
 
-    public init(@RequestBuilder params: () -> RequestParam) {
-        self.params = params().unzip.map {
-            guard
-                let FormParam = $0 as? FormParam,
-                !(FormParam is Form)
-            else {
-                fatalError()
-            }
-
-            return FormParam
-        }
+    public init(@FormBuilder params: () -> FormParam) {
+        self.params = params().unzip
     }
 
     public func buildData(_ data: inout Foundation.Data, with boundary: String) {
---
+++ Sources/Request/Request/FormParam/FormBuilder.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//  
+//
+//  Created by brennobemoura on 18/11/20.
+//
+
+import Foundation
+
+@_functionBuilder
+struct FormBuilder {
+    public static func buildBlock(_ params: FormParam...) -> FormParam {
+        Combined(params)
+    }
+
+    public static func buildBlock(_ param: FormParam) -> FormParam {
+        param
+    }
+
+    public static func buildIf(_ param: FormParam?) -> FormParam {
+        param ?? Empty()
+    }
+
+    public static func buildEither(first: FormParam) -> FormParam {
+        first
+    }
+
+    public static func buildEither(second: FormParam) -> FormParam {
+        second
+    }
+}
+
+private extension FormBuilder {
+    struct Combined: FormParam {
+        let children: [FormParam]
+
+        init(_ children: [FormParam]) {
+            self.children = children
+        }
+
+        func buildData(_ data: inout Data, with boundary: String) {
+            fatalError()
+        }
+    }
+}
+
+private extension FormBuilder {
+    struct Empty: FormParam {
+        func buildData(_ data: inout Data, with boundary: String) {}
+    }
+}
+
+extension FormParam {
+    var unzip: [FormParam] {
+        if let combined = self as? FormBuilder.Combined {
+            return combined.children
+        }
+
+        return [self]
+    }
+}
