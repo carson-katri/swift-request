c2d9f1ec5e3ec211b6c7a151169cdc2f1b794794
Carson Katri <carson.katri@gmail.com>
2021-01-17 17:02:11 -0500

    Rework RequestImage implementation

--- Sources/Request/SwiftUI/Views/RequestImage.swift
+++ Sources/Request/SwiftUI/Views/RequestImage.swift
@@ -12,9 +12,9 @@ import SwiftUI
 /// It automatically has animations to transition from a placeholder and the image.
 ///
 /// It takes a `Url` or `Request`, a placeholder, the `ContentMode` for displaying the image, and an `Animation` for switching
-public struct RequestImage: View {
+public struct RequestImage<Placeholder: View>: View {
     private let request: Request
-    private let placeholder: Image
+    private let placeholder: Placeholder
     private let animation: Animation
     private let contentMode: ContentMode
     #if os(OSX)
@@ -23,68 +23,66 @@ public struct RequestImage: View {
     @State private var image: UIImage? = nil
     #endif
     
-    #if os(OSX)
-    public init(_ url: Url, placeholder: Image = Image(nsImage: NSImage()), contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
-        self.request = Request {
-            url
-        }
-        self.placeholder = placeholder
+    public init(_ url: Url, @ViewBuilder placeholder: () -> Placeholder, contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
+        self.request = Request { url }
+        self.placeholder = placeholder()
         self.animation = animation
         self.contentMode = contentMode
     }
+    
+    public var body: some View {
+        Group {
+            if let image = image {
+                #if os(OSX)
+                Image(nsImage: image)
+                    .resizable()
+                #else
+                Image(uiImage: image)
+                    .resizable()
+                #endif
+            } else {
+                placeholder
+                    .onAppear {
+                        self.request.onData { data in
+                            #if os(OSX)
+                            self.image = NSImage(data: data)
+                            #else
+                            self.image = UIImage(data: data)
+                            #endif
+                        }
+                        .call()
+                    }
+            }
+        }
+        .aspectRatio(contentMode: contentMode)
+        .animation(animation)
+    }
+}
+
+extension RequestImage where Placeholder == Image {
+    #if os(OSX)
+    public init(_ url: Url, placeholder: Image = Image(nsImage: NSImage()), contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
+        self.init(Request { url }, placeholder: placeholder, contentMode: contentMode, animation: animation)
+    }
     #else
     public init(_ url: Url, placeholder: Image = Image(uiImage: UIImage()), contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
-        self.request = Request {
-            url
-        }
-        self.placeholder = placeholder
-        self.animation = animation
-        self.contentMode = contentMode
+        self.init(Request { url }, placeholder: placeholder, contentMode: contentMode, animation: animation)
     }
     #endif
     
     #if os(OSX)
     public init(_ request: Request, placeholder: Image = Image(nsImage: NSImage()), contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
         self.request = request
-        self.placeholder = placeholder
+        self.placeholder = placeholder.resizable()
         self.animation = animation
         self.contentMode = contentMode
     }
     #else
     public init(_ request: Request, placeholder: Image = Image(uiImage: UIImage()), contentMode: ContentMode = .fill, animation: Animation = .easeInOut) {
         self.request = request
-        self.placeholder = placeholder
+        self.placeholder = placeholder.resizable()
         self.animation = animation
         self.contentMode = contentMode
     }
     #endif
-    
-    public var body: some View {
-        if image != nil {
-            #if os(OSX)
-            return Image(nsImage: image!)
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .animation(animation)
-            #else
-            return Image(uiImage: image!)
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .animation(animation)
-            #endif
-        } else {
-            self.request.onData { data in
-                #if os(OSX)
-                self.image = NSImage(data: data)
-                #else
-                self.image = UIImage(data: data)
-                #endif
-            }
-            .call()
-            return placeholder
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .animation(animation)
-        }
-    }
 }
